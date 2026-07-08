e5de8dfb18f3d92514615ce20e2bfeb0baf80905
Alexis Lapasset <alexis.lapasset@gmail.com>
2026-07-08 08:51:30 +0200

    chore(release): bump version to 0.1.21 (versionCode 21)

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: 'Kiddo',
     slug: 'kiddo',
-    version: '0.1.20',
+    version: '0.1.21',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'dark',
@@ -23,7 +23,7 @@ export default {
         backgroundColor: '#18181e',
       },
       package: 'io.kiddo.app',
-      versionCode: 20,
+      versionCode: 21,
       googleServicesFile: './google-services.json',
       permissions: ['android.permission.RECORD_AUDIO'],
     },
